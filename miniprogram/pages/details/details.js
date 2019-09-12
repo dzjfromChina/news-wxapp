699bd7476091ba6add3693109f35b2f60d54fa05
duzj <dzj0574@163.com>
2019-09-12 22:19:10 +0800

    详情页1%
    详情页1%

--- miniprogram/pages/details/details.js
+++ miniprogram/pages/details/details.js
@@ -5,14 +5,24 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    news:{}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var id = options.id 
+    console.log("index传过来的Id："+id)
+    const db = wx.cloud.database()
+    // 查询当前用户所有的 counters
+    db.collection('newses').doc(id).get({
+      success: function (res) {
+        // res.data 包含该记录的数据
+        console.log(res.data)
+      }
+    })
+    console.log("end")
   },
 
   /**
--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -6,6 +6,7 @@ Page({
    */
   data: {
     newses: [{
+      "id":"0",
       "title": "1",
       "main": "2",
       "people": "3",
@@ -16,86 +17,97 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
-     var that = this;
+  onLoad: function (options) {
+    var that = this;
     that.data.newses = [];
-    var newNewses = []; 
-     const db = wx.cloud.database()
-     // 查询当前用户所有的 counters
-     db.collection('newses').get({
-       success: res => {
-         var dataDB = res.data;
-         for (let i = 0; i < dataDB.length; ++i) {
-           console.log(dataDB[i])
-           var news = {
-             "title": dataDB[i].title,
-             "main": dataDB[i].main,
-             "people": dataDB[i].peopleOk+" 位用户觉得赞",
-             "mt": dataDB[i].mt
-           }
-           newNewses.push(news)
-         }
-         this.setData({
-           newses: newNewses
-         })
-         console.log('[数据库] [查询记录] 成功: ', res)
+    var newNewses = [];
+    const db = wx.cloud.database()
+    // 查询当前用户所有的 counters
+    db.collection('newses').get({
+      success: res => {
+        var dataDB = res.data;
+        for (let i = 0; i < dataDB.length; ++i) {
+          //console.log(dataDB[i]._id)
+          var news = {
+            "id": dataDB[i]._id,
+            "title": dataDB[i].title,
+            "main": dataDB[i].main,
+            "people": dataDB[i].peopleOk + " 位用户觉得赞",
+            "mt": dataDB[i].mt
+          }
+          newNewses.push(news)
+        }
+        this.setData({
+          newses: newNewses
+        })
+        console.log('[数据库] [查询记录] 成功: ', res)
       },
       fail: err => {
         wx.showToast({
           icon: 'none',
-           title: '查询记录失败'
+          title: '查询记录失败'
         })
         console.error('[数据库] [查询记录] 失败：', err)
-       }
-     })
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {
+  onReady: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
+  onShow: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {
+  onHide: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {
+  onUnload: function () {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
+  onReachBottom: function () {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
+  onShareAppMessage: function () {
 
+  },
+
+  /**
+   * 跳转到新闻详情页面
+   */
+  detail: function (e) {
+    console.log( e)
+    wx.navigateTo({
+      url: '/pages/details/details?id=' + e.currentTarget.dataset.id,
+    })
   }
 })
